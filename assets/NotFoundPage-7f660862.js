084accff63c35c528a59c859f0563a9bf75013df
vitaliy1990 <chumakov_vitaliy@meta.ua>
2023-09-14 10:14:24 +0300

    Updates

---
+++ assets/NotFoundPage-7f660862.js
@@ -0,0 +1,1 @@
+import{r as s,j as o}from"./index-55338b49.js";const t={},e=()=>o.jsx("div",{className:t.root,children:o.jsx("h1",{children:"404 Page not found"})}),n=s.memo(e);export{n as default};
